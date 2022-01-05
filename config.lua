e2e979237b4ab08fd558b0f15d29ac87765bf27c
Andrey Loginov <dukelog@Andreys-MacBook-Pro.local>
2022-01-05 22:14:41 +0500

    add treesitter folding

--- config.lua
+++ config.lua
@@ -123,6 +123,11 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+-- Folding
+vim.opt.foldmethod='expr'
+vim.opt.foldexpr='nvim_treesitter#foldexpr()'
+vim.opt.foldenable=false
+
 -- generic LSP settings
 
 -- ---@usage disable automatic installation of servers
